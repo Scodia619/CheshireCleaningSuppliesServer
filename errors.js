53e6cf878c3d05b7a7818283d8b4c5bdd20f75b3
Scodia619 <billyjoe2701@gmail.com>
2024-09-29 22:39:07 +0100

    Fix?

---
+++ errors.js
@@ -0,0 +1,11 @@
+exports.customErrors = (err, req, res, next) => {
+    if (err.status && err.msg) {
+      res.status(err.status).send({ msg: err.msg });
+    }else next(err);
+  };
+
+  exports.prismaErrors = (err, req, res, next) => {
+    if (err.code === 'P2003') res.status(400).send({ msg: "Bad Request - Data Needed or Topic / Commission doesnt exist" })
+    else if(err.code === 'P2025') res.status(404).send({msg: "No commission found"})
+    next(err);
+  };
